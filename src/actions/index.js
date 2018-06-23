40186551144661efda01cc826ee4e64d956135a8
Adam Rogers <cldd5678@hotmail.com>
2018-06-23 16:12:28 -0400

    add action types and creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,21 @@
 import actionTypes from "./actionTypes";
 
+export const addDirection = (recipeId, direction) => ({
+	type: actionTypes.ADD_DIRECTION,
+	payload: {
+		direction: direction,
+		recipeId: recipeId
+	}
+});
+
+export const addIngredient = (recipeId, ingredient) => ({
+	type: actionTypes.ADD_INGREDIENT,
+	payload: {
+		ingredient: ingredient,
+		recipeId: recipeId
+	}
+});
+
 export const addRecipe = recipe => ({
 	type: actionTypes.ADD_RECIPE,
 	recipe: recipe
@@ -7,6 +23,10 @@ export const addRecipe = recipe => ({
 
 export const beginAddRecipe = () => ({
 	type: actionTypes.BEGIN_ADD_RECIPE
+});
+
+export const beginEditRecipe = () => ({
+	type: actionTypes.BEGIN_EDIT_RECIPE
 });
 
 export const cancelAddRecipe = () => ({
@@ -18,9 +38,59 @@ export const changeCreatingRecipe = recipe => ({
 	recipe: recipe
 });
 
+export const changeDirection = (recipeId, directionId, newDirection) => ({
+	type: actionTypes.CHANGE_DIRECTION,
+	payload: {
+		directionId: directionId,
+		newDirection: newDirection,
+		recipeId: recipeId
+	}
+});
+
+export const changeIngredient = (recipeId, ingredientId, newIngredient) => ({
+	type: actionTypes.CHANGE_INGREDIENT,
+	payload: {
+		ingredientId: ingredientId,
+		newIngredient: newIngredient,
+		recipeId: recipeId
+	}
+});
+
 export const changePage = pageNum => ({
 	type: actionTypes.CHANGE_PAGE,
 	pageNum: pageNum
+});
+
+export const changeRecipeDescription = (recipeId, description) => ({
+	type: actionTypes.CHANGE_RECIPE_DESCRIPTION,
+	payload: {
+		description: description,
+		recipeId: recipeId
+	}
+});
+
+export const changeRecipeName = (recipeId, name) => ({
+	type: actionTypes.CHANGE_RECIPE_NAME,
+	payload: {
+		name: name,
+		recipeId: recipeId
+	}
+});
+
+export const deleteDirection = (recipeId, directionId) => ({
+	type: actionTypes.DELETE_DIRECTION,
+	payload: {
+		directionId: directionId,
+		recipeId: recipeId
+	}
+});
+
+export const deleteIngredient = (recipeId, ingredientId) => ({
+	type: actionTypes.DELETE_INGREDIENT,
+	payload: {
+		ingredientId: ingredientId,
+		recipeId: recipeId
+	}
 });
 
 export const deleteRecipe = id => ({
@@ -28,9 +98,45 @@ export const deleteRecipe = id => ({
 	id: id
 });
 
+export const endEditRecipe = () => {
+	type: actionTypes.END_EDIT_RECIPE;
+};
+
 export const initRecipes = recipes => ({
 	type: actionTypes.INIT_RECIPES,
 	recipes: recipes
+});
+
+export const moveDirectionDown = (recipeId, directionId) => ({
+	type: actionTypes.MOVE_DIRECTION_DOWN,
+	payload: {
+		directionId: directionId,
+		recipeId: recipeId
+	}
+});
+
+export const moveDirectionUp = (recipeId, directionId) => ({
+	type: actionTypes.MOVE_DIRECTION_UP,
+	payload: {
+		directionId: directionId,
+		recipeId: recipeId
+	}
+});
+
+export const moveIngredientDown = (recipeId, ingredientId) => ({
+	type: actionTypes.MOVE_INGREDIENT_DOWN,
+	payload: {
+		ingredientId: ingredientId,
+		recipeId: recipeId
+	}
+});
+
+export const moveIngredientUp = (recipeId, ingredientId) => ({
+	type: actionTypes.MOVE_INGREDIENT_UP,
+	payload: {
+		ingredientId: ingredientId,
+		recipeId: recipeId
+	}
 });
 
 export const searchRecipeNames = search => ({
