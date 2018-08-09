3cee3a10d67010c04ac5e972e24d4b7153c13e51
Adam Rogers <cldd5678@hotmail.com>
2018-08-09 16:11:11 -0400

    Update to only connect in non-test environments

--- src/server/db/mongoose.js
+++ src/server/db/mongoose.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI);
+if (process.env.NODE_ENV !== 'test') {
+  mongoose.connect(process.env.MONGODB_URI);
+}
 
 module.exports = {
   mongoose
