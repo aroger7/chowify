06bcdad71ecdbe9e2d7a90030537c2f58e4df0db
Adam Rogers <cldd5678@hotmail.com>
2018-08-16 17:15:37 -0400

    Add cors

--- src/server/app.js
+++ src/server/app.js
@@ -3,20 +3,29 @@ require('./config/config');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const cors = require('cors');
 
 const { mongoose } = require('./db/mongoose');
 const users = require('./routes/users');
 const recipes = require('./routes/recipes');
 
 const app = express();
-const port = process.env.PORT;
 
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    allowedHeaders: ['x-auth', 'Content-Type'],
+    exposedHeaders: ['x-auth']
+  })
+);
 app.use(express.static(path.resolve(__dirname, '../../dist')));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.render(path.resolve(__dirname, '../../dist/index.html'));
-});
+if (process.env.NODE_ENV !== 'development') {
+  app.get('/', (req, res) => {
+    res.render(path.resolve(__dirname, '../../dist/index.html'));
+  });
+}
 
 app.use('/users', users);
 app.use('/recipes', recipes);
