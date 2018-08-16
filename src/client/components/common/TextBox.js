e03b4b7409d33f4a7f76ca51c963ba7b20b9887e
Adam Rogers <cldd5678@hotmail.com>
2018-08-16 17:29:06 -0400

    Update default TextBox styling

--- src/client/components/common/TextBox.js
+++ src/client/components/common/TextBox.js
@@ -3,7 +3,9 @@ import styled from 'styled-components';
 const TextBox = styled.input.attrs({
   type: 'text'
 })`
-    font: 1em 'PT Sans', sans-serif;
+  border: none;
+  border-bottom: 2px solid black;
+  font: 1em 'PT Sans', sans-serif;
 `;
 
 export default TextBox;
